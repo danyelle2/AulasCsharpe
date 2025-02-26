be5baa032e70dceea32a44d45229619269510796
danyelle2 <dany.alvesbrito2@gmail.com>
2025-02-26 14:48:51 -0300

    upload dos códigos da primeira aula

---
+++ Aula1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 39;
+            string nomeCompleto = "Danyelle";
+
+            var variavelSemTipo = "Danyelle";
+
+            Console.WriteLine(variavelSemTipo.GetType());
+
+            Console.WriteLine("Digite seu nome: ");
+            string nomeUsuario =Console.ReadLine();
+            Console.WriteLine("Digite a sua idade: ");
+            string idadeUsuario = Console.ReadLine();
+
+
+            Console.WriteLine($"Meu nome é {nomeCompleto} e a minha idade é {idadeUsuario}");
+        }
+    }
+}
